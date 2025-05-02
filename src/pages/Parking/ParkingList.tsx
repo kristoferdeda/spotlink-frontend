0b65f511c16547b0246d942ec29d45e3fd815177
kristoferdeda <kristi.deda15@gmail.com>
2025-05-02 14:42:19 -0400

    Update ParkingList.tsx

--- src/pages/Parking/ParkingList.tsx
+++ src/pages/Parking/ParkingList.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth } from "../../context/AuthContext";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://spotlink-backend.onrender.com/api";
 
 interface ParkingSpot {
   _id: string;
