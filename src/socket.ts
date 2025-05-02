44ebe52877ac13af28bcc7514a883e0ed3e0c2fb
kristoferdeda <kristi.deda15@gmail.com>
2025-05-02 14:39:52 -0400

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://spotlink-backend.onrender.com/", {
+const socket = io("https://spotlink-backend.onrender.com/api", {
   withCredentials: true,
 });
 
